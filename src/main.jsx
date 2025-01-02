eefdead72fc05af81fce05c0b4859c3ce85ae155
malfinowildanakhya <johndoe@example.com1112022468@mhs.dinus.ac.id>
2025-01-03 03:29:41 +0700

    Fix: correct import path for AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeContextProvider } from './context/themeContext.jsx'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
